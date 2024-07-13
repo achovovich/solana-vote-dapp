83dda0b4e36561817a54484d5807eb7222ce3762
Olivier <achovovich@gmail.com>
2024-07-13 19:23:52 +0200

    Space Add Form

--- app/components/Cards/SpaceAdd.jsx
+++ app/components/Cards/SpaceAdd.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-import { useForm } from 'react-hook-form';
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, } from "@/components/ui/card"
@@ -8,14 +7,14 @@ import { Label } from "@/components/ui/label"
 import CustomCardHeader from "./CardHeader"
 import { Textarea } from "@/components/ui/textarea"
 
-import { z } from "zod"
-
 export default function SpaceAdd() {
 
-    const { handleSubmit, register, errors } = useForm();
-    const onSubmit2 = (data) => {
-        console.log(`input value: ${data.name}`);
-        console.table(data);
+    const [spaceName, setName] = useState('');
+    const [spaceList, setSpaceList] = useState('');
+
+    const createSpace = () => {
+        console.log(spaceName);
+        console.log(spaceList);
     }
 
     return (
@@ -23,21 +22,21 @@ export default function SpaceAdd() {
             <CustomCardHeader title={"Créez un espace de vote"} description={''}></CustomCardHeader>
 
             <CardContent>
-                <form onSubmit={handleSubmit(onSubmit2)} >
+                <form  > {/* onSubmit={handleSubmit(onSubmit2)} */}
                     <div className="grid w-full items-center gap-4">
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="name">Nom de l'espace</Label>
-                            <Input id="name" placeholder="Titre l'espace de vote" ref={register()} />
+                            <Input id="name" placeholder="Titre l'espace de vote" onChange={(e) => setName(e.target.value)} />
                             {/* {errors.name && <span>The email is required</span>} */}
                         </div>
                         <Label htmlFor="whitelist">Adresses authorisées à voter:</Label>
-                        <Textarea placeholder="1 adresse par ligne" id="whitelist" />
-                    </div>
-                    <div className="flex justify-between flex-row-reverse mt-4">
-                        <Button type="submit">Creer</Button>
+                        <Textarea placeholder="1 adresse par ligne" id="whitelist" onChange={(e) => setSpaceList(e.target.value)} />
                     </div>
                 </form>
             </CardContent>
+            <CardFooter className="flex justify-between flex-row-reverse">
+                <Button onClick={createSpace}>Creer</Button>
+            </CardFooter>
         </Card>
     )
 }
