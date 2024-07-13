d001d26e6868ca1d2a8dcd3574e3d2c7ca17ff08
wwwookiee <jordandivita@gmail.com>
2024-07-13 14:34:38 +0200

    base program file system added

--- programs/solvote/src/lib.rs
+++ programs/solvote/src/lib.rs
@@ -2,6 +2,13 @@ use anchor_lang::prelude::*;
 
 declare_id!("4Cd7TvNJyjF2DFuWeKrMDJWqFtzCSH7Ry2VVM8E2DyeN");
 
+pub mod constants;
+pub mod errors;
+pub mod instructions;
+pub mod state;
+
+use instructions::*;
+
 #[program]
 pub mod solvote {
     use super::*;
