fdcbeb31f9e18f526ec4f16a598ea3739a4cee28
Olivier <achovovich@gmail.com>
2024-07-13 19:07:24 +0200

    Spaces list

---
+++ app/pages/spaces/list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../layout';
+import SpacesList from '@/components/Spaces/List';
+
+export default function SpacesListPage() {
+
+    return (
+        <Layout>
+
+            <SpacesList />
+        </Layout>
+    );
+};
